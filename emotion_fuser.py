4891117341188c7eeef398750b00085cbb5f34f6
Kaylie Stuteville <140630741+Kstuteville@users.noreply.github.com>
2025-12-04 13:21:24 -0500

    Changes post playtest

--- emotion_fuser.py
+++ emotion_fuser.py
@@ -38,7 +38,7 @@ if not os.path.exists(LOG_FILE):
 llm_client = OpenAI(api_key=os.getenv("NPC_AI_KEY"))
 
 # CONFIGvvvvvv
-UE_IP = "192.168.1.159"
+UE_IP = ""
 UE_PORT = 9000
 client = udp_client.SimpleUDPClient(UE_IP, UE_PORT)
 
@@ -449,4 +449,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-#
